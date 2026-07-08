3fceabbe0f7172a176f40e63c72f63e8f469c397
Eva Alonso <evam.alonso95@gmail.com>
2026-07-08 19:37:49 +0200

    Primer ws desde postman

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log("hola mundo");
+import { log } from "node:console";
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on("connection", function connection(ws) {
+  console.log("Client connect");
+
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+  });
+
+  ws.send("Hola desde el servidor");
+
+  setInterval(() => {
+    ws.send("Hola de nuevo");
+  }, 2000);
+});
+
+console.log("Server running on port http://localhost:3000");

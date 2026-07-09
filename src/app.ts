bea7e51e7391432910e09a7c5964a35145c35747
Eva Alonso <evam.alonso95@gmail.com>
2026-07-09 15:49:10 +0200

    Implementar lógica para broadcast

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { log } from "node:console";
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 3000 });
 
@@ -9,14 +9,19 @@ wss.on("connection", function connection(ws) {
   ws.on("error", console.error);
 
   ws.on("message", function message(data) {
-    console.log("Desde el cliente", data);
-
     // Enviamos la data recibida del cliente
-    const payload = {
+    const payload = JSON.stringify({
       type: "custom-message",
       payload: data.toString(),
-    };
-    ws.send(JSON.stringify(payload));
+    });
+    // ws.send(JSON.stringify(payload));
+
+    //Broadcast
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(payload, { binary: false });
+      }
+    });
   });
 
   //   ws.send("Hola desde el servidor");
